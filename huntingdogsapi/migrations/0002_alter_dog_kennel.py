5f7c8ab65c2493a99e0460d472766ec02aa6daf5
Sam Barker <barkersam29@yahoo.com>
2023-03-01 17:04:26 -0600

    migrations

--- huntingdogsapi/migrations/0002_alter_dog_kennel.py
+++ huntingdogsapi/migrations/0002_alter_dog_kennel.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='dog',
             name='kennel',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='dogs', to='huntingdogsapi.kennel'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                    related_name='dogs', to='huntingdogsapi.kennel'),
         ),
     ]
